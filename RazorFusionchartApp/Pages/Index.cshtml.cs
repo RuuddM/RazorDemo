95dd68c968d46550537f88e12168c621263f70e7
Ruud <ruud.mulleman@hotmail.com>
2021-05-27 19:07:42 +0200

    Create dataset

--- RazorFusionchartApp/Pages/Index.cshtml.cs
+++ RazorFusionchartApp/Pages/Index.cshtml.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using FusionCharts.DataEngine;
+using FusionCharts.Visualization;
+using System.Data;
 
 namespace RazorFusionchartApp.Pages {
     public class IndexModel : PageModel {
@@ -18,6 +21,28 @@ namespace RazorFusionchartApp.Pages {
         // When the page is being loaded, OnGet method will be  invoked
         public string ChartJson { get; internal set; }
         public void OnGet() {
+
+            // create data table to store data
+            DataTable ChartData = new DataTable();
+            // Add columns to data table
+            ChartData.Columns.Add("Neigborhood", typeof(System.String));
+            ChartData.Columns.Add("Number of listing", typeof(System.Double));
+            // Add rows to data table
+
+            ChartData.Rows.Add("Bijlmer-Centrum", 100);
+            ChartData.Rows.Add("Bijlmer-Oost", 98);
+            ChartData.Rows.Add("Centrum-Oost", 1610);
+            ChartData.Rows.Add("Centrum-West", 2179);
+            ChartData.Rows.Add("Noord-Oost", 218);
+            ChartData.Rows.Add("Noord-West", 262);
+
+            // Create static source with this data table
+            StaticSource source = new StaticSource(ChartData);
+            // Create instance of DataModel class
+            DataModel model = new DataModel();
+            // Add DataSource to the DataModel
+            model.DataSources.Add(source);
+
 
         }
     }
